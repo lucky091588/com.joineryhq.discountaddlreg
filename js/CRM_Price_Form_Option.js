fe1e7e0c29692bfc4ba746f21eb5ff1596a88e74
Allen Shaw <allen@JoineryHQ.com>
2020-05-11 09:48:54 -0500

    Added descriptions to fields on Price Option edit form.

--- js/CRM_Price_Form_Option.js
+++ js/CRM_Price_Form_Option.js
@@ -13,29 +13,33 @@
         $('input#discountaddlreg_is_active').closest('tr').nextAll('tr').hide();
       }
     };
-    
+
     // Give the bhfe elements table an id so we can handle it later.
     $('input#discountaddlreg_is_active').closest('table').attr('id', 'bhfe_table');
     $('table#bhfe_table').removeClass('form-layout-compressed');
     $('table#bhfe_table').addClass('form-layout');
     $('table#bhfe_table td.nowrap').removeClass('nowrap');
-    
+
     var mainTable = $('input#label').closest('table');
-    
+
     // Insert a fieldset after mainTable, and move bhfe table into that.
     mainTable.after('<fieldset id="discountaddlreg_fieldset"><legend>' + ts('Discounts for additional participants') + '</legend></fieldset>');
-    
+
     myTable = mainTable.clone();
     myTable.attr('id', 'discountaddlreg_table');
     myTable.find('tbody').empty();
     $('fieldset#discountaddlreg_fieldset').append(myTable);
     for (var i in CRM.vars.discountaddlreg.fieldNames) {
-      console.log('label[for="' + CRM.vars.discountaddlreg.fieldNames[i] +'"]');
       myTable.find('tbody').append($('table#bhfe_table').find('label[for="' + CRM.vars.discountaddlreg.fieldNames[i] +'"]').closest('tr'));
     }
-    
+
     $('input#discountaddlreg_is_active').change(discountaddlregIsActiveChange);
     discountaddlregIsActiveChange();
-    
+
+    // Append any descriptions for bhfe fields.
+    for (var d in CRM.vars.discountaddlreg.descriptions) {
+      $('input#' + d + ', select#' + d).after('<div class="description" id="' + d + '-description">'+ CRM.vars.discountaddlreg.descriptions[d] +'</div>');
+    }
+
   });
 }(CRM.ts('com.joineryhq.discountaddlreg')));
